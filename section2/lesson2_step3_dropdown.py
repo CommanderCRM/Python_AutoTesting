afb30dc7362e4ced58f56c0f8077274285833be4
Ilya Krivoshein <htctomsk634@gmail.com>
2022-11-24 12:56:20 +0700

    renamed files, added new

---
+++ section2/lesson2_step3_dropdown.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+try: 
+    link = "https://suninjuly.github.io/selects1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    num1 = browser.find_element(By.CSS_SELECTOR, "#num1")
+    num2 = browser.find_element(By.CSS_SELECTOR, "#num2")
+    sum = int(num1.text) + int(num2.text)
+    select = Select(browser.find_element(By.CSS_SELECTOR, "#dropdown"))
+    select.select_by_value(str(sum))
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
