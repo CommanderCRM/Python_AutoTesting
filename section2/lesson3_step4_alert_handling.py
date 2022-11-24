f0e866cd22e41e4f4384fe7e66288406e3d818dd
Ilya Krivoshein <htctomsk634@gmail.com>
2022-11-24 14:01:14 +0700

    new file

---
+++ section2/lesson3_step4_alert_handling.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # автоматическое получение численного ответа
+    alert = browser.switch_to.alert
+    alert_text = alert.text
+    alert.accept()
+    alert_number = float(alert_text.split(': ')[-1])
+    print(alert_number)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
